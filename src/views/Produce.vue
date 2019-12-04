c70cdcff0d38e8eb425f6408da5bcfd7a3535c07
moling <365024424@qq.com>
2019-12-04 13:21:58 +0800

    完成生产看板

--- src/views/Produce.vue
+++ src/views/Produce.vue
@@ -1,23 +1,44 @@
 <template>
   <div id="Produce">
-    <!--  -->
-    Produce
+    <h1 class="title">{{ model.title }}</h1>
+    <v-table
+      style="width: 100%;"
+      class="main-table"
+      :columns="columns"
+      :table-data="tableData"
+      :show-vertical-border="false"
+      is-horizontal-resize
+      :vertical-resize-offset="60"
+      :multiple-sort="false"
+      :min-height="450"
+      :title-row-height="54"
+      :row-height="48"
+    />
   </div>
 </template>
 
 <script>
 import api from '@/api'
+import Produce from '@/models/Produce'
 
 export default {
   name: 'Produce',
   data () {
     return {
       timer: null,
+      model: Produce,
+      tableData: [],
     }
   },
   computed: {
     wsCode () {
       return this.$route.query.wsCode
+    },
+    columns () {
+      return this.model.cols.map(col => {
+        col.isResize = true
+        return col
+      })
     },
   },
   mounted () {
@@ -38,8 +59,8 @@ export default {
       if (!this.wsCode) {
         return void alert('缺少参数wsCode')
       }
-      api.get('ProductReport/wsCodeProduct', { wsCode: this.wsCode }).then(data => {
-        console.log(data)
+      api.get('ProduceReport', { wsCode: this.wsCode }).then(data => {
+        this.tableData = data
         this.timer = setTimeout(this.fetchReportData, 5000)
       })
     },
@@ -47,6 +68,22 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.title {
+  text-align: center;
+}
+.main-table {
+  margin: 5px auto;
+  font-size: 20px;
+  .v-table-header-row {
+    background-color: #efeeec;
+    font-weight: bold;
+    & td:first-child > div {
+      margin-left: 3px;
+    }
+  }
+}
+.v-table-row td:first-child > div {
+  margin-left: 3px;
+}
 </style>
