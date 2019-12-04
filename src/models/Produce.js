abb704d771eae442b163f6f5a32652ddcc162f07
moling <365024424@qq.com>
2019-12-04 13:21:20 +0800

    调整生产看板的模型

--- src/models/Produce.js
+++ src/models/Produce.js
@@ -2,16 +2,16 @@ export default {
   // 模型名称
   name: 'Produce',
   // 标题
-  title: '车间生产数据',
+  title: '车间生产看板',
   // 表格列配置
   cols: [
-    { field: 'lineName', title: '线别', width: 100 },
+    { field: 'lineName', title: '线别', width: 120 },
     { field: 'customer', title: '客户', width: 150 },
     { field: 'orderNo', title: '工单', width: 150 },
-    { field: 'productCode', title: '产品型号', width: 150 },
-    { field: 'qty', title: '计划数量', width: 100 },
-    { field: 'cpltQty', title: '完成数量', width: 100 },
-    { field: 'failQty', title: '不良品', width: 100 },
-    { field: 'reach', title: '达成率(%)', width: 100 },
+    { field: 'productName', title: '产品型号', width: 300 },
+    { field: 'qty', title: '计划数量', width: 70 },
+    { field: 'cpltQty', title: '完成数量', width: 70 },
+    { field: 'failQty', title: '不良品', width: 70 },
+    { field: 'reach', title: '达成率(%)', width: 70 },
   ],
 }
