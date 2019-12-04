9c20ba1d9ac58c28bf3ed331871c1901860581ea
moling <365024424@qq.com>
2019-12-04 10:35:12 +0800

    生产看板通过api获取展示数据

--- src/views/Produce.vue
+++ src/views/Produce.vue
@@ -6,11 +6,42 @@
 </template>
 
 <script>
+import api from '@/api'
+
 export default {
   name: 'Produce',
+  data () {
+    return {
+      timer: null,
+    }
+  },
   computed: {
     wsCode () {
       return this.$route.query.wsCode
+    },
+  },
+  mounted () {
+    this.fetchReportData()
+  },
+  beforeDestroy () {
+    this._clear()
+  },
+  methods: {
+    _clear () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+        this.timer = null
+      }
+    },
+    fetchReportData () {
+      this._clear()
+      if (!this.wsCode) {
+        return void alert('缺少参数wsCode')
+      }
+      api.get('ProductReport/wsCodeProduct', { wsCode: this.wsCode }).then(data => {
+        console.log(data)
+        this.timer = setTimeout(this.fetchReportData, 5000)
+      })
     },
   },
 }
