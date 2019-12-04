88d7964d98071a1ddd7dddcf900a827e238a2370
moling <365024424@qq.com>
2019-12-04 10:53:13 +0800

    导入vue-easytable

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+// 导入 table 组件
+import 'vue-easytable/libs/themes-base/index.css'
+import { VTable } from 'vue-easytable'
+Vue.component(VTable.name, VTable)
 
 Vue.config.productionTip = false
 
